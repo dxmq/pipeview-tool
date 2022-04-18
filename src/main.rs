54faa8892541d98e5684c969c4af064a051e152c
zcg <1697859639@qq.com>
2022-04-18 20:27:40 +0800

    使用crossbeam重构

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
+use crossbeam::channel::{bounded, unbounded};
 use std::io::Result;
-use std::sync::mpsc;
 use std::thread;
 
 use eminent_spoon::{args::Args, read, stats, write};
 
 fn main() -> Result<()> {
-    let (stats_tx, stats_rx) = mpsc::channel();
-    let (writer_tx, write_rx) = mpsc::channel();
+    let (stats_tx, stats_rx) = unbounded();
+    let (write_tx, write_rx) = bounded(1024);
 
     let args = Args::parse();
     let Args {
@@ -15,8 +15,8 @@ fn main() -> Result<()> {
         silent,
     } = args;
 
-    let read_handle = thread::spawn(move || read::read_loop(&infile, stats_tx));
-    let stats_handle = thread::spawn(move || stats::stats_loop(silent, stats_rx, writer_tx));
+    let read_handle = thread::spawn(move || read::read_loop(&infile, stats_tx, write_tx));
+    let stats_handle = thread::spawn(move || stats::stats_loop(silent, stats_rx));
     let write_handle = thread::spawn(move || write::write_loop(&outfile, write_rx));
 
     let read_io_result = read_handle.join().unwrap();
--- src/stats.rs
+++ src/stats.rs
@@ -1,22 +1,24 @@
+use crossbeam::channel::Receiver;
 use std::io::Result;
-use std::sync::mpsc::Receiver;
-use std::sync::mpsc::Sender;
+use std::time::Instant;
 
-pub fn stats_loop(
-    silent: bool,
-    stats_rx: Receiver<Vec<u8>>,
-    write_tx: Sender<Vec<u8>>,
-) -> Result<()> {
+pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
+    let start = Instant::now();
+    let mut last_instant = Instant::now();
     let mut total_bytes = 0;
     loop {
-        let buffer = stats_rx.recv().unwrap();
-        let nums_bytes = buffer.len();
+        let nums_bytes = stats_rx.recv().unwrap();
+        let now = Instant::now();
+        let rate_per_second = nums_bytes as f64 / (now - last_instant).as_secs_f64();
+        last_instant = now;
         total_bytes += nums_bytes;
         if !silent {
-            eprint!("\r{}", total_bytes);
-        }
-        if write_tx.send(buffer).is_err() {
-            break;
+            eprint!(
+                "\r{} {} [{:.0}b/s]",
+                total_bytes,
+                start.elapsed().as_secs(),
+                rate_per_second
+            );
         }
         if nums_bytes == 0 {
             break;
