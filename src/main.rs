658ca58c8bad876de6b085700ef4b47334652cc3
zcg <1697859639@qq.com>
2022-04-18 19:57:25 +0800

    使用 mpsc channel 重构

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
 use std::io::Result;
+use std::sync::mpsc;
+use std::thread;
+
 use eminent_spoon::{args::Args, read, stats, write};
 
-
 fn main() -> Result<()> {
-    let mut total_bytes = 0;
+    let (stats_tx, stats_rx) = mpsc::channel();
+    let (writer_tx, write_rx) = mpsc::channel();
+
     let args = Args::parse();
-    loop {
-        let buffer = match read::read(&args.infile) {
-            Ok(x) if x.is_empty() => break,
-            Ok(x) => x,
-            Err(_) => break,
-        };
-        stats::stats(args.silent, buffer.len(), &mut total_bytes, false);
-        if !write::write(&args.outfile, &buffer)? {
-            break;
-        }
-    }
-    stats::stats(args.silent, 0, &mut total_bytes, true);
+    let Args {
+        infile,
+        outfile,
+        silent,
+    } = args;
+
+    let read_handle = thread::spawn(move || read::read_loop(&infile, stats_tx));
+    let stats_handle = thread::spawn(move || stats::stats_loop(silent, stats_rx, writer_tx));
+    let write_handle = thread::spawn(move || write::write_loop(&outfile, write_rx));
+
+    let read_io_result = read_handle.join().unwrap();
+    let stats_result = stats_handle.join().unwrap();
+    let write_io_result = write_handle.join().unwrap();
+
+    read_io_result?;
+    stats_result?;
+    write_io_result?;
     Ok(())
 }
--- src/stats.rs
+++ src/stats.rs
@@ -1,11 +1,29 @@
+use std::io::Result;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
 
-pub fn stats(silent: bool, num_read:usize, total_bytes: &mut usize, last: bool) {
-    *total_bytes += num_read;
-    if !silent {
-        eprint!("\r{}", total_bytes);
-        if last {
-            eprintln!();
+pub fn stats_loop(
+    silent: bool,
+    stats_rx: Receiver<Vec<u8>>,
+    write_tx: Sender<Vec<u8>>,
+) -> Result<()> {
+    let mut total_bytes = 0;
+    loop {
+        let buffer = stats_rx.recv().unwrap();
+        let nums_bytes = buffer.len();
+        total_bytes += nums_bytes;
+        if !silent {
+            eprint!("\r{}", total_bytes);
+        }
+        if write_tx.send(buffer).is_err() {
+            break;
+        }
+        if nums_bytes == 0 {
+            break;
         }
     }
-
+    if !silent {
+        eprintln!();
+    }
+    Ok(())
 }
